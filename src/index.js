8868390505f7c33e96ded4377132781d471f094b
Dibbayajyoti <125145390+DibbayajyotiRoy@users.noreply.github.com>
2024-01-14 22:38:23 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,37 +1,19 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
-})
-
-
-
-
-
+  path: "./env",
+});
 
 connectDB()
 .then(() => {
-  app.listen(process.env.PORT || 5000, ()=>{
-    console.log(`Server is running at port:${process.env.PORT}`);
-  })
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
 .catch((err) => {
-  console.log("MONGO db connection failed !!!", err);
+    console.log("MONGO db connection failed !!! ", err);
 })
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 import express from "express";
--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,16 +1,14 @@
-import mmulter from "multer"
-
+import mmulter from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-     
-      cb(null, file.filename)
-    }
-  })
-  
-export const upload = multer({ 
-storage, 
-})
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.filename);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
